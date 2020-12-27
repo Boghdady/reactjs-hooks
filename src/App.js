86ad371d5a66f7aa2ab847c2e81bf10e9315ee61
boghdady <boghdady107@gmail.com>
2020-12-28 01:39:04 +0200

    useEffect() hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 function App() {
@@ -8,6 +8,10 @@ function App() {
   // 2. should exist on the function not a class component
   // 3. should not exist on loop, conditions, etc.
   
+  // 1. useState() hook
+  // when path funcation as arguement in useState() this mean
+  // the useState() will render only once not render everytime 
+  // when state is changes. that is good for performance wise.
   const [count, setCount] = useState(() => 0);
   function increment() {
     setCount(prevCount => prevCount+1);
@@ -15,6 +19,16 @@ function App() {
   function decrement() {
     setCount(prevCount => prevCount-1);
   }
+
+  // 2. useEffect(a,b)
+  // first arg is a function that run when the second arg "b" change
+  // b = [] mean run a only once
+  // don't forget to cleanup "performace wise"
+  useEffect(() => {
+    console.log("useEffect")
+    return () => console.log("Cleanup")
+  },[count])
+
   return (
     <div className="App">
      <header className="App-header">
