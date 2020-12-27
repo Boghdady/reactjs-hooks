b62699828a473954215ae1e69f6fbe818ce2706e
boghdady <boghdady107@gmail.com>
2020-12-28 00:45:46 +0200

    useState() hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
 
+import React, { useState } from 'react';
+
 
 function App() {
+  // Hooks : 1. should exist on the top of function
+  // 2. should exist on the function not a class component
+  // 3. should not exist on loop, conditions, etc.
+  
+  const [count, setCount] = useState(() => 0);
+  function increment() {
+    setCount(prevCount => prevCount+1);
+  }
+  function decrement() {
+    setCount(prevCount => prevCount-1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <div>+</div>
-        <span>0</span>
-        <div>-</div>
+     <header className="App-header">
+        <button onClick={increment}>+</button>
+        <span>{count}</span>
+        <button onClick={decrement}>-</button>
       </header>
     </div>
   );
